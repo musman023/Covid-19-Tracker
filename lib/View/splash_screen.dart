ceedba5aa65e713bffdc5d595d6c3dc723c7b98c
Muhammad Usman <163582723+musman023@users.noreply.github.com>
2024-03-19 15:58:10 +0500

    Splash screen

--- lib/View/splash_screen.dart
+++ lib/View/splash_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+import 'package:flutter/widgets.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -7,7 +10,11 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: const Duration(seconds: 3), vsync: this)
+        ..repeat();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +22,33 @@ class _SplashScreenState extends State<SplashScreen> {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [],
+        children: [
+          AnimatedBuilder(
+              animation: _controller,
+              child: Container(
+                width: 200,
+                height: 200,
+                child: const Center(
+                  child: Image(image: AssetImage('assets/virus.png')),
+                ),
+              ),
+              builder: (BuildContext context, Widget? child) {
+                return Transform.rotate(
+                  angle: _controller.value * 2.0 * math.pi,
+                  child: child,
+                );
+              }),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          const Align(
+              alignment: Alignment.center,
+              child: Text(
+                "Covid-19\nTracker App",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              )),
+        ],
       )),
     );
   }
